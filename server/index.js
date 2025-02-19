e0497cfd520303afdfad36b4eaf4e268d4953465
Pranjul Chaurasiya <chaurasiyapranjul@gmail.com>
2025-02-19 22:44:24 +0530

    user-route created

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
+
+const cors = require("cors");
+const cookieparser = require("cookie-parser");
+const { userRouter } = require("./router/user-router");
 const app =   express();
 require("./db")
 dotenv.config();
+
+
+app.use(cors({credentials:true}));
+
+app.use(cookieparser());
+
+app.use("/api/user",userRouter);
+
 app.use("/api",(req,res)=>{
     res.status(200).json({
         message:"hello express"
     })
 })
+
+app.use("/api")
 
 app.listen(5000,()=>{
     console.log("server is running at port 5000");
---
+++ server/router/user-router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { registerUser } = require("../controllers/user-controller");
+
+const userRouter = express.Router();
+
+
+
+
+userRouter.post("/register",registerUser);
+
+module.exports = {
+    userRouter
+}
