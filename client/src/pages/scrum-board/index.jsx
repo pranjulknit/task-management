c912f1d3f96897e214c44393e86341561f056820
Pranjul Chaurasiya <chaurasiyapranjul@gmail.com>
2025-03-05 13:18:05 +0530

    scrum-board started

--- client/src/pages/scrum-board/index.jsx
+++ client/src/pages/scrum-board/index.jsx
@@ -1,5 +1,47 @@
+import { scrumBoardOptions } from "@/config";
+import { TaskManagerContext } from "@/context";
+import { useContext, useEffect } from "react";
+
 function ScrumBoardPage(){
-    return <div>ScrumPage</div>
+    
+    const {user,tasksList,setTasksList} = useContext(TaskManagerContext);
+
+    // can be use a hook to get the user
+      async function fetchListOfTasks() {
+        console.log(user, "user hai");
+        setLoading(true);
+        const response = await getAllTasksApi(user?._id);
+        if (response?.success) {
+          console.log(response);
+          setTasksList(response?.allusersbyId);
+          setLoading(false);
+        }
+      }
+    useEffect(()=>{
+        if(user !== null)
+      fetchListOfTasks()      
+    },[user])
+    return <>
+        <div className="grid grid-cols-5 gap-2 h-full">
+            {
+                scrumBoardOptions.map(item => <div key={item.id} className="border border-[#333333] rounded overflow-auto "> 
+
+                    <div className="px-1 py-3 text-center bg-black border-none mb-3 ">
+                        <h3 className="text-2xl font-extrabold text-white">
+                            {item.label}
+                        </h3>
+                    </div>
+
+                    <div>
+                        {
+                            renderTodoStatus
+                        }
+                    </div>
+
+                </div> )
+            }
+        </div>
+    </>
 }
 
 export default ScrumBoardPage;
